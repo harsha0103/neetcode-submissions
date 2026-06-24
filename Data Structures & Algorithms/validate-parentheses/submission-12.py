aef9eae40bc5145b122e70f900db8db7d140b878
harsha0103 <nirmalharsha0103@gmail.com>
2026-06-23 22:40:57 -0700

    Add: validate-parentheses - submission-12

---
+++ Data Structures & Algorithms/validate-parentheses/submission-12.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        arr=[]
+
+        for i in s: 
+            if i =='(' or i=='{' or i=='[':
+                arr.append(i)
+            else:
+                res=arr.pop()
+                if (i==')' and res!='(') or (i=='}' and res!='{') or (i==']' and res!='[') :
+                    return False
+        return True if len(arr)==0 else False
+                    
