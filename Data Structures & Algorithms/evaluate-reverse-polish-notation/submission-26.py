d809fd105c963cab50c536961849d5dcc578044b
harsha0103 <nirmalharsha0103@gmail.com>
2026-06-24 17:53:34 -0700

    Add: evaluate-reverse-polish-notation - submission-26

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-26.py
@@ -0,0 +1,20 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        mapping ={
+            '+': lambda a,b:a+b,
+            '-': lambda a,b:a-b,
+            '*': lambda a,b:a*b,
+            '/': lambda a,b:int(float(a)/b)
+        }
+        stack=[]
+        for i in tokens:
+            if i in mapping:
+               b=stack.pop() 
+               a=stack.pop()
+               res=mapping[i](a,b)
+               stack.append(res)
+            else:
+                stack.append(int(i))
+        
+        final=stack.pop()
+        return final
